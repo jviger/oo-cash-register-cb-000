daacab7788f318e19cc1cdab6e9fb7cf7a87a476
Jason Viger <jason.viger@gmail.com>
2018-08-19 08:13:09 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   end
   
   def add_item(title, price,quantity=1)
-    title.times do |x|
+    quantity.times do |x|
       @items.push(x)
     end
     @total += price * quantity
